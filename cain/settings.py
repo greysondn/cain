58e2273265fdf8af7286d3fd756b25fda987dacb
J. Dorian Greyson L <greysondn@gmail.com>
2020-03-21 01:34:15 +0000

    cain - urls and settings, guess that means this is set for my home service, oh no

--- cain/settings.py
+++ cain/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = ')^r(jb!y7p*5!j-*nom^pffu#)((+4_*zsc0vxwx1tdw+u%(uo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]", "192.168.254.21", "192.168.254.13"]
 
 # Application definition
 
